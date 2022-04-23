b7fe85ff398517919382cf0f1f009c35cc144f5a
yahya <yah1381yahya81@gmail.com>
2022-04-23 08:31:02 -0700

    resources completed

--- projectCode/src/main/java/Model/LuxuryResource.java
+++ projectCode/src/main/java/Model/LuxuryResource.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class LuxuryResource extends Resource{
 
 
@@ -7,9 +9,9 @@ public class LuxuryResource extends Resource{
 
 
     ////methods////
-    public LuxuryResource(int food, int production, int gold, String constructionNeeded)
+    public LuxuryResource(int food, int production, int gold, String constructionNeeded, ArrayList<String> landsThatCanBeFound)
     {
-        super(food, production, gold, constructionNeeded);
+        super(food, production, gold, constructionNeeded, landsThatCanBeFound);
     }
 
 
--- projectCode/src/main/java/Model/LuxuryResourceTypes.java
+++ projectCode/src/main/java/Model/LuxuryResourceTypes.java
@@ -1,23 +1,29 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum LuxuryResourceTypes {
-    COTTON(0, 0, 2, "FARMHOUSE"),
-    COLOR(0, 0, 2, "FARMHOUSE"),
-    FUR(0, 0, 2 , "CAMP"),
-    GEMSTONE(0, 0, 3, "MINE"),
-    GOLD(0, 0, 2, "MINE"),
-    INCENSE(0, 0, 2, "FARMHOUSE"),
-    IVORY(0, 0, 2, "CAMP"),
-    MARBLE(0, 0, 2, "STONE_MINE"),
-    SILK(0, 0, 2, "FARMHOUSE"),
-    SILVER(0, 0, 2, "MINE"),
-    SUGAR(0, 0, 2, "FARMHOUSE");
+    COTTON(0, 0, 2, "FARMHOUSE", new ArrayList<String>(Arrays.asList("PLAIN", "DESERT", "GRASSLAND"))),
+    COLOR(0, 0, 2, "FARMHOUSE", new ArrayList<String>(Arrays.asList("JUNGLE", "DENSE_FOREST"))),
+    FUR(0, 0, 2 , "CAMP", new ArrayList<String>(Arrays.asList("JUNGLE", "TUNDRA"))),
+    GEMSTONE(0, 0, 3, "MINE",
+            new ArrayList<String>(Arrays.asList("DENSE_FOREST", "TUNDRA", "GRASSLAND", "PLAIN", "DESERT", "HILLS"))),
+    GOLD(0, 0, 2, "MINE", new ArrayList<String>(Arrays.asList("PLAIN", "DESERT", "GRASSLAND", "HILLS"))),
+    INCENSE(0, 0, 2, "FARMHOUSE", new ArrayList<String>(Arrays.asList("PLAIN", "DESERT"))),
+    IVORY(0, 0, 2, "CAMP", new ArrayList<String>(List.of("PLAIN"))),
+    MARBLE(0, 0, 2, "STONE_MINE",
+            new ArrayList<String>(Arrays.asList("TUNDRA", "PLAIN", "DESERT", "GRASSLAND", "HILLS"))),
+    SILK(0, 0, 2, "FARMHOUSE", new ArrayList<String>(List.of("JUNGLE"))),
+    SILVER(0, 0, 2, "MINE", new ArrayList<String>(Arrays.asList("TUNDRA", "DESERT", "HILLS"))),
+    SUGAR(0, 0, 2, "FARMHOUSE", new ArrayList<String>(Arrays.asList("FLOOD_PLAIN", "SWAMP")));
 
 
     final LuxuryResource resource;
 
-    LuxuryResourceTypes(int food, int production, int gold, String constructionNeeded)
+    LuxuryResourceTypes(int food, int production, int gold, String constructionNeeded, ArrayList<String> landsThatCanBeFound)
     {
-        resource = new LuxuryResource(food, production, gold, constructionNeeded);
+        resource = new LuxuryResource(food, production, gold, constructionNeeded, landsThatCanBeFound);
     }
 }
