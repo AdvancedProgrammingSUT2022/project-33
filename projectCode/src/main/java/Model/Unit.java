b85d379ea545bc285b52d5dcccb3d877161d18f3
yahya <yah1381yahya81@gmail.com>
2022-04-25 12:21:16 -0700

    semi Unit added

---
+++ projectCode/src/main/java/Model/Unit.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Unit {
+    String type;
+    int health;
+    int visibilityRange;
+    int maxMovements;
+    int remainingMovements;
+    Coordinates coordinates;
+    Coordinates destinationCoordinates;
+    int price;
+    int timeRequiredForBeingMade;
+    int maintenancePricePerTurn;
+
+
+
+
+
+    ////methods////
+    public Unit()
+    {
+        //TODO:
+    }
+}
