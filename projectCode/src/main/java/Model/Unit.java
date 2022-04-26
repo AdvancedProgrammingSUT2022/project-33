fe326c25a508ab0e2994887779ea1d3c3f03a324
yahya <yah1381yahya81@gmail.com>
2022-04-26 14:47:35 -0700

    unit updated

--- projectCode/src/main/java/Model/Unit.java
+++ projectCode/src/main/java/Model/Unit.java
@@ -13,6 +13,9 @@ public class Unit {
     int price;
     int timeRequiredForBeingMade;
     int maintenancePricePerTurn;
+    boolean isMoving;
+    PathFinder pathFinder;
+    ArrayList<Coordinates> path;
 
 
 
@@ -28,6 +31,7 @@ public class Unit {
         this.price = price;
         this.timeRequiredForBeingMade = timeRequiredForBeingMade;
         this.maintenancePricePerTurn = maintenancePricePerTurn;
+        this.path = new ArrayList<>();
 
 
     }
@@ -41,20 +45,69 @@ public class Unit {
 
 
 
-    private void moveUnit()
+    public void resetUnit()
     {
-        if (coordinates.equals(destinationCoordinates)){
-            return;
-        }
         //TODO:
-
     }
 
 
 
-//    private ArrayList<Coordinates> findTheBestRoot(ArrayList<Terrain> terrains, ArrayList<Coordinates> fogCoordinates)
-//    {
-//
-//        //TODO:
-//    }
+    private void moveUnit(ArrayList<Terrain> terrains)
+    {
+        ArrayList<Coordinates> remainingPath = new ArrayList<>();
+
+        int i = 0;
+
+        while(!path.get(i).equals(coordinates)){
+            i++;
+        }
+
+        i++;
+
+        while (i < path.size()){
+            remainingPath.add(path.get(i));
+        }
+
+        i = 0;
+
+        while (!coordinates.equals(destinationCoordinates) && remainingMovements >= getTerrainFromCoordinates(terrains, path.get(i)).getMovementPrice()){
+            remainingMovements -= getTerrainFromCoordinates(terrains, path.get(i)).getMovementPrice();
+            coordinates = path.get(i);
+            i++;
+        }
+
+
+        if (coordinates.equals(destinationCoordinates)){
+            isMoving = false;
+        }
+    }
+
+
+
+    public void setPath(Coordinates destinationCoordinates)
+    {
+        this.destinationCoordinates = destinationCoordinates;
+
+        pathFinder.findPath(coordinates, destinationCoordinates);
+    }
+
+
+
+    private Terrain getTerrainFromCoordinates(ArrayList<Terrain> terrains, Coordinates coordinates)
+    {
+        for (int i = 0; i < terrains.size(); i++){
+            if (coordinates.equals(terrains.get(i))){
+                return terrains.get(i);
+            }
+        }
+
+        return null;
+    }
+
+
+
+    public void cancelCurrentAction()
+    {
+        //TODO:
+    }
 }
