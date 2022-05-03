f0ca628f1a8a59d1b9ccd972273f7175231fc31f
yahya <yah1381yahya81@gmail.com>
2022-05-03 03:22:40 -0700

    Ancient buildings added

--- projectCode/src/main/java/Model/Building.java
+++ projectCode/src/main/java/Model/Building.java
@@ -3,18 +3,21 @@ package Model;
 public class Building {
     String gameName;
     int cost;
+    int productionNeeded;
     int maintenance;
     int foodPerTurn;
     int productionPurTurn;
     int happinessPurTurn;
     int goldPurTurn;
-    int sciencePurTurn;
+    int sciencePurTwoCitizen;
+    int scienceEffect;
     int defenceEffect;
     int unitsXpBonus;
     Technologies technologyNeeded;
     BuildingTypes RequiredBuildings;
     boolean needsRiver;
     boolean needsHorse;
+    Eras era;
 
 
 
@@ -22,22 +25,25 @@ public class Building {
 
     ////methods////
 
-    public Building(String gameName, int cost, int maintenance, int foodPerTurn, int productionPurTurn,
-                    int happinessPurTurn, int goldPurTurn, int sciencePurTurn, int defenceEffect, int unitsXpBonus,
-                    Technologies technologyNeeded, BuildingTypes requiredBuildings, boolean needsRiver, boolean needsHorse) {
+    public Building(String gameName, int cost, int productionNeeded, int maintenance, int foodPerTurn, int productionPurTurn,
+                    int happinessPurTurn, int goldPurTurn, int sciencePurTwoCitizen, int scienceEffect, int defenceEffect, int unitsXpBonus,
+                    Technologies technologyNeeded, BuildingTypes requiredBuildings, boolean needsRiver, boolean needsHorse, Eras era) {
         this.gameName = gameName;
         this.cost = cost;
+        this.productionNeeded  = productionNeeded;
         this.maintenance = maintenance;
         this.foodPerTurn = foodPerTurn;
         this.productionPurTurn = productionPurTurn;
         this.happinessPurTurn = happinessPurTurn;
         this.goldPurTurn = goldPurTurn;
-        this.sciencePurTurn = sciencePurTurn;
+        this.sciencePurTwoCitizen = sciencePurTwoCitizen;
+        this.scienceEffect = scienceEffect;
         this.defenceEffect = defenceEffect;
         this.unitsXpBonus = unitsXpBonus;
         this.technologyNeeded = technologyNeeded;
-        RequiredBuildings = requiredBuildings;
+        this.RequiredBuildings = requiredBuildings;
         this.needsRiver = needsRiver;
         this.needsHorse = needsHorse;
+        this.era = era;
     }
 }
--- projectCode/src/main/java/Model/BuildingTypes.java
+++ projectCode/src/main/java/Model/BuildingTypes.java
@@ -2,6 +2,22 @@ package Model;
 
 public enum BuildingTypes {
     //Ancient Buildings
+    BARRACKS("Barracks", 80,  75, 1, 0, 0, 0, 0, 0,
+            0, 0, 15, Technologies.BRONZE_WORKING, null, false, false, Eras.ANCIENT),
+    GRANARY("Granary", 100, 85, 1, 2, 0, 0, 0, 0,
+            0, 0, 0, Technologies.POTTERY, null, false, false, Eras.ANCIENT),
+    LIBRARY("Library", 80, 90, 1, 0, 0, 0, 0, 1,
+            0, 0, 0, Technologies.WRITING, null, false, false, Eras.ANCIENT),
+    MONUMENT("Monument", 60, 75, 1, 0, 1, 0, 0, 0,
+            0, 0, 0, null, null, false, false, Eras.ANCIENT),
+    WALLS("Walls", 100, 100, 1, 0, 0, 0, 0,0 ,
+            0, 5, 0, Technologies.MASONRY, null, false, false, Eras.ANCIENT),
+    WATER_MILL("Watter Mill", 120, 110, 2, 2, 1, 0, 0, 0,
+            0, 0, 0, Technologies.THE_WHEEL, null, true, false, Eras.ANCIENT),
+
+    //Classical Buildings
+    
+
 
 
     ;
@@ -9,11 +25,11 @@ public enum BuildingTypes {
 
     final Building building;
 
-    BuildingTypes(String gameName, int cost, int maintenance, int foodPerTurn, int productionPurTurn,
-                  int happinessPurTurn, int goldPurTurn, int sciencePurTurn, int defenceEffect, int unitsXpBonus,
-                  Technologies technologyNeeded, BuildingTypes requiredBuildings, boolean needsRiver, boolean needsHorse)
+    BuildingTypes(String gameName, int cost, int productionNeeded, int maintenance, int foodPerTurn, int productionPurTurn,
+                  int happinessPurTurn, int goldPurTurn, int sciencePurTwoCitizen, int scienceEffect, int defenceEffect, int unitsXpBonus,
+                  Technologies technologyNeeded, BuildingTypes requiredBuildings, boolean needsRiver, boolean needsHorse, Eras era)
     {
-        building  = new Building(gameName, cost, maintenance, foodPerTurn, productionPurTurn, happinessPurTurn, goldPurTurn,
-                sciencePurTurn, defenceEffect, unitsXpBonus, technologyNeeded, requiredBuildings, needsRiver, needsHorse);
+        building  = new Building(gameName, cost, productionNeeded, maintenance, foodPerTurn, productionPurTurn, happinessPurTurn, goldPurTurn,
+                sciencePurTwoCitizen, scienceEffect, defenceEffect, unitsXpBonus, technologyNeeded, requiredBuildings, needsRiver, needsHorse, era);
     }
 }
